77396a165ef608acab81b12bfee259c0d37fbf20
Kim Corson <kimmc67b@gmail.com>
2024-02-25 16:25:43 -0600

    game text revert for playing

--- DungeonBattlesOfWinterspellCpp/GameText.cpp
+++ DungeonBattlesOfWinterspellCpp/GameText.cpp
@@ -4,41 +4,41 @@
 #include <conio.h>
 
 void GameText::WriteLineInput(std::string text) {
-	//for (char character : text) {
-	//	std::cout << character;
-	//	std::cout.flush(); // Flush the output to ensure immediate display
-	//	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
-	//}
-	//std::cout << '\n';
+	for (char character : text) {
+		std::cout << character;
+		std::cout.flush(); // Flush the output to ensure immediate display
+		std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
+	}
+	std::cout << '\n';
 
-	//_getch();
-	//system("cls");
-
-	std::cout << text << std::endl;
 	_getch();
 	system("cls");
+
+	/*std::cout << text << std::endl;
+	_getch();
+	system("cls");*/
 }
 
 void GameText::WriteLine(std::string text) {
-	//for (char character : text) {
-	//	std::cout << character;
-	//	std::cout.flush(); // Flush the output to ensure immediate display
-	//	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
-	//}
-	//std::cout << '\n';
+	for (char character : text) {
+		std::cout << character;
+		std::cout.flush(); // Flush the output to ensure immediate display
+		std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
+	}
+	std::cout << '\n';
 
-	std::cout << text << std::endl;
+	//std::cout << text << std::endl;
 }
 
 
 void GameText::WriteText(std::string text) {
-	//for (char character : text) {
-	//	std::cout << character;
-	//	std::cout.flush(); // Flush the output to ensure immediate display
-	//	std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
-	//}
+	for (char character : text) {
+		std::cout << character;
+		std::cout.flush(); // Flush the output to ensure immediate display
+		std::this_thread::sleep_for(std::chrono::milliseconds(1)); // chrono lib for time duration
+	}
 
-	std::cout << text;
+	//std::cout << text;
 }
 
 void GameText::WriteWithoutTyping(std::string text) {
